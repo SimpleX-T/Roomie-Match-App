ee18c618d034df08b86db41253744def168e5fb4
Xcodes <ntmark2004@gmail.com>
2024-03-21 16:01:10 +0100

    splitting components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-
 import "./App.css";
 import LandingPage from "./Components/LandingPage/LandingPage";
-import SignUp from "./Components/SignIn/SignupPage";
+import SignUp from "./Components/SignIn/SignUpPage";
 import SignIn from "./Components/SignIn/SignInPage";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -12,9 +12,10 @@ export default function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<LandingPage />} />
-					<Route path='/login' element={<SignUp />} />
-					<Route path='/signup' element={<SignUp />} />
 					<Route path='/login' element={<SignIn />} />
+					<Route path='/new' element={<SignUp />} />
+					<Route path='/login' element={<SignIn />} />
+					<Route path='*' element={<ErrorPage />} />
 				</Routes>
 			</BrowserRouter>
 			{/* <LandingPage /> */}
